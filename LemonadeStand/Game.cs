66fff7a56f3a4288ab4cd8efea1d78d154bf0e53
Brandies7 <adanb82@gmail.com>
2017-08-09 13:51:19 -0500

    created cups,icecubes, lemons, and sugar classes. also added rules to game class and added chooseruntime to player class

--- LemonadeStand/Game.cs
+++ LemonadeStand/Game.cs
@@ -8,9 +8,19 @@ namespace LemonadeStand
 {
     class Game
     {
-        public Game()
+        Player player;
+        private Game()
         {
 
         }
+
+        private void DisplayRules()
+        {
+            Console.WriteLine("Welocme " + player.name + ". Congratulations on your new buisness venture. Lets go over the rules and where to begin" + Environment.NewLine +
+                              "First, you'll need to go to the store and pick up your products. Then you're going to have to decide how to make your lemonade" + Environment.NewLine +
+                              "(How much of each ingredient to add). Things you need to keep in mind: The weather will affect your sales. Price will also" + Environment.NewLine +
+                              "affect your sales. The taste of your lemonade will affect customer satisfaction. Customer satisfaction will affect your stands" + Environment.NewLine +
+                              "popularity. Well, that pretty much covers it. Good luck" + player.name + "and may your stand turn a nice profit.");
+         }
     }
 }
