5be82b90578ecdff3b59a472f17690c84b92015f
Brandies7 <adanb82@gmail.com>
2017-08-11 13:44:49 -0500

    added getOpinions method to the Customer class

--- LemonadeStand/Customer.cs
+++ LemonadeStand/Customer.cs
@@ -30,9 +30,23 @@ namespace LemonadeStand
 
         }
 
+        private string getOpinions()
+        {
+            List<string> customerOpinions = new List<string>();
+            customerOpinions.Add("I'm really thirsty, no matter what the weather today, I'm buying some lemonade.");
+            customerOpinions.Add("I hope it's nice outside today so I can buy some lemonade.");
+            customerOpinions.Add("I feel like some lemonade, but if it's raining, I won't even be able to stop to get some. I don't want to be stuck in the rain.");
+            customerOpinions.Add("If it is hot out, I'm definitely stop and get some lemonade.");
+            Random random = new Random();
+            int opinion = random.Next(customerOpinions.Count);
+            string todaysOpinion = customerOpinions[opinion];
+            return todaysOpinion;
+        }
+
         public void runCustomers()
         {
             getCustomers();
+            getOpinions();
         }
 
     }
